b2a8c3302fc64cab210f809c1e788f7812f69fd1
Guillem <saguesguillem@gmail.com>
2022-11-15 18:58:05 +0100

    get dom elements

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function main() {
   console.log("test");
-
+  // Variables
   let turnoX = 1;
   const arraySeleccionesX = [
     false,
@@ -48,6 +48,29 @@ function main() {
   const fichas = ["O", "X"];
   let fichasPuestas = 0;
   let finPartida = false;
+
+  // DOM elements.
+
+  const botona1 = document.getElementsByClassName("a1-button")[0];
+  const botona2 = document.getElementsByClassName("a2-button")[0];
+  const botona3 = document.getElementsByClassName("a3-button")[0];
+  const botonb1 = document.getElementsByClassName("b1-button")[0];
+  const botonb2 = document.getElementsByClassName("b2-button")[0];
+  const botonb3 = document.getElementsByClassName("b3-button")[0];
+  const botonc1 = document.getElementsByClassName("c1-button")[0];
+  const botonc2 = document.getElementsByClassName("c2-button")[0];
+  const botonc3 = document.getElementsByClassName("c3-button")[0];
+  const arrButtonElements = [
+    botona1,
+    botona2,
+    botona3,
+    botonb1,
+    botonb2,
+    botonb3,
+    botonc1,
+    botonc2,
+    botonc3,
+  ];
   console.log("selecciones X", arraySeleccionesX);
   function handleclick() {
     // comprobar si la casilla ya a sido seleccionada
@@ -63,16 +86,12 @@ function main() {
       case "X":
         // 1.guardar la informacion.
         arraySeleccionesX[0] = true;
+
         console.log("selecciones X", arraySeleccionesX);
         // 2.colocar la ficha X en UI
-        const boton1 = document.getElementsByClassName("a1-button")[0];
-        boton1.innerHTML = "X";
-        boton1.style.background = "#d0d0d0";
-        // const boton2 = document.getElementsByClassName("a2-button")[1];
-        // boton2.innerHTML = "X";
-        // boton2.style.background = "#d0d0d0";
+
         // comprobar victoria
-        arraySeleccionesX == combinacionesGanadoras;
+        // arraySeleccionesX == combinacionesGanadoras;
         // 3. cambio de turno
         turno = "O";
 
@@ -80,16 +99,16 @@ function main() {
 
       // SELECCION O
       case "O":
+        arraySeleccionesO[0] = true;
+        botona1 = document.getElementsByClassName("a1-button")[0];
+        botona1.innerHTML = "O";
+        botona1.style.background = "#F8C471";
         console.log("turno", turno);
         // arraySeleccionesO[]
         break;
     }
     console.log("click");
   }
-  const boton1 = document.getElementsByClassName("a1-button")[0];
-  boton1.addEventListener("click", handleclick);
-
-  console.log("boton1", boton1);
 }
 main();
 // window.addEventListener("load", main);
@@ -102,10 +121,3 @@ main();
 //   if (condition) {
 //   }
 // }
-switch (key) {
-  case value:
-    break;
-
-  default:
-    break;
-}
